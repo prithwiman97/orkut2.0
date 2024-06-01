4a6b3dd50c76fdccc6c38cc16c9a35dd33ef49bd
Prithwiman Mazumdar <30746565+prithwiman97@users.noreply.github.com>
2024-06-01 07:52:28 +0530

    Initial commit

---
+++ orkut2.0.Contracts/Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace orkut2._0.Contracts.Models
+{
+    public class Users
+    {
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int? PhoneNumber { get; set; }
+    }
+}
---
+++ orkut2.0.Rest/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using orkut2._0.Contracts.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace orkut2._0.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new List<Users> { new() { Firstname = "Prithwiman", Lastname = "Mazumdar" } });
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
